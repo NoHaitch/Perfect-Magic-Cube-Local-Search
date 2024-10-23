682664548af4d3afffc740155c0ab16b9f987465
Francisco <franciscotrianto@gmail.com>
2024-10-23 08:59:58 +0700

    feat: get side diagonals and is_perfect function

--- src/data_structure/magic_cube.py
+++ src/data_structure/magic_cube.py
@@ -17,9 +17,8 @@ class MagicCube:
         :param size: Magic Cube Dimensions, default = 5
         """
         self.size: int = size                                                       # dimensions, default 5x5x5
-        # self.data: np.ndarray = np.random.permutation(np.arange(1, size**3 + 1))    # default is 1 to 125
+        self.data: np.ndarray = np.random.permutation(np.arange(1, size**3 + 1))    # default is 1 to 125
         self.magic_sum: int = size * (size**3 + 1) // 2                             # default is 315
-        self.data: np.ndarray = np.arange(1, size**3 + 1)
         # magic_sum is the magic number/constant
 
     def __str__(self):
@@ -28,28 +27,13 @@ class MagicCube:
         """
         return self.data.reshape(self.size, self.size, self.size).__str__()
 
-    def _get_index(self, x: int, y: int, z: int) -> int:
-        """
-        Returns the 1D index for the given x, y, z coordinates in the Magic Cube.
-
-        :return: The corresponding index in the 1D array.
-        """
-        if x < 0 or x >= self.size:
-            raise IndexError("Row index out of bounds")
-        if y < 0 or y >= self.size:
-            raise IndexError("Column index out of bounds")
-        if z < 0 or z >= self.size:
-            raise IndexError("Layer index out of bounds")
-
-        return x + y * self.size + z * self.size**2
-
     def get_row(self, y: int, z: int) -> np.ndarray:
         """
         Returns the specified row.
 
         :return: The row at y and z
         """
-        first_index = self._get_index(0, y, z)
+        first_index = self.__get_index(0, y, z)
         return self.data[first_index:first_index+self.size]
 
     def get_col(self, x: int, z: int) -> np.ndarray:
@@ -61,7 +45,7 @@ class MagicCube:
         :return: The column at x and z.
         """
         # Calculate the starting index of the specified column in the given layer
-        start_index = self._get_index(x, 0, z)
+        start_index = self.__get_index(x, 0, z)
         # Use slicing to return the column as a NumPy array
         return self.data[start_index:start_index + self.size**2:self.size]
 
@@ -90,16 +74,16 @@ class MagicCube:
         for i in range(self.size):
             if start_from_left and start_from_front:
                 # From (0, 0, 0) to (n-1, n-1, n-1)
-                diagonal[i] = self.data[self._get_index(i, i, i)]
+                diagonal[i] = self.data[self.__get_index(i, i, i)]
             elif start_from_left and not start_from_front:
                 # From (0, n-1, 0) to (n-1, 0, n-1)
-                diagonal[i] = self.data[self._get_index(i, self.size - 1 - i, i)]
+                diagonal[i] = self.data[self.__get_index(i, self.size - 1 - i, i)]
             elif not start_from_left and start_from_front:
                 # From (n-1, 0, 0) to (0, n-1, n-1)
-                diagonal[i] = self.data[self._get_index(self.size - 1 - i, i, i)]
+                diagonal[i] = self.data[self.__get_index(self.size - 1 - i, i, i)]
             else:
                 # From (n-1, n-1, 0) to (0, 0, n-1)
-                diagonal[i] = self.data[self._get_index(self.size - 1 - i, self.size - 1 - i, i)]
+                diagonal[i] = self.data[self.__get_index(self.size - 1 - i, self.size - 1 - i, i)]
 
         return diagonal
 
@@ -116,6 +100,126 @@ class MagicCube:
 
         return np.array(diagonals)
 
-    # TODO: Get Side Diagonal for each axis
+    def get_side_diags_x(self) -> np.ndarray:
+        """
+        Return all side diagonals from the Magic Cube at x-axis as a 2D array.
+        """
+        left_diags = [self.__get_side_diag_x_left(i) for i in range(self.size)]
+        right_diags = [self.__get_side_diag_x_right(i) for i in range(self.size)]
 
-    # TODO: is_perfect(self) -> bool:
+        # Combine left and right diagonals into a single 2D array
+        return np.array(left_diags + right_diags)
+
+    def get_side_diags_y(self) -> np.ndarray:
+        """
+        Return all side diagonals from the Magic Cube at y-axis as a 2D array.
+        """
+        left_diags = [self.__get_side_diag_y_left(i) for i in range(self.size)]
+        right_diags = [self.__get_side_diag_y_right(i) for i in range(self.size)]
+
+        return np.array(left_diags + right_diags)
+
+    def get_side_diags_z(self) -> np.ndarray:
+        """
+        Return all side diagonals from the Magic Cube at z-axis as a 2D array.
+        """
+        left_diags = [self.__get_side_diag_z_left(i) for i in range(self.size)]
+        right_diags = [self.__get_side_diag_z_right(i) for i in range(self.size)]
+
+        return np.array(left_diags + right_diags)
+
+    def is_perfect(self) -> bool:
+        """
+        Checks if the Magic Cube is a perfect magic cube.
+
+        A perfect magic cube has the same magic sum for:
+            - All rows
+            - All columns
+            - All pillars
+            - All space diagonals
+            - All side diagonals
+
+        :return: True if the cube is perfect, False otherwise.
+        """
+
+        # Check all rows
+        for z in range(self.size):
+            for y in range(self.size):
+                if np.sum(self.get_row(y, z)) != self.magic_sum:
+                    print(self.get_row(y, z))
+                    print("Row", y, z, "sum is", np.sum(self.get_row(y, z)), "expected", self.magic_sum)
+                    return False
+
+        # Check all columns
+        for z in range(self.size):
+            for x in range(self.size):
+                if np.sum(self.get_col(x, z)) != self.magic_sum:
+                    print("Col", x, z, "sum is", np.sum(self.get_col(x, z)), "expected", self.magic_sum)
+                    return False
+
+        # Check all pillars
+        for y in range(self.size):
+            for x in range(self.size):
+                if np.sum(self.get_pillar(x, y)) != self.magic_sum:
+                    print("Pillar", x, y, "sum is", np.sum(self.get_pillar(x, y)), "expected", self.magic_sum)
+                    return False
+
+        # Check all space diagonals
+        for diag in self.get_space_diags():
+            if np.sum(diag) != self.magic_sum:
+                print("Space Diag", diag, "sum is", np.sum(diag), "expected", self.magic_sum)
+                return False
+
+        # Check all side diagonals on each axis
+        for diag_x in self.get_side_diags_x():
+            if np.sum(diag_x) != self.magic_sum:
+                print("Side Diag X", diag_x, "sum is", np.sum(diag_x), "expected", self.magic_sum)
+                return False
+
+        for diag_y in self.get_side_diags_y():
+            if np.sum(diag_y) != self.magic_sum:
+                print("Side Diag Y", diag_y, "sum is", np.sum(diag_y), "expected", self.magic_sum)
+                return False
+
+        for diag_z in self.get_side_diags_z():
+            if np.sum(diag_z) != self.magic_sum:
+                print("Side Diag Z", diag_z, "sum is", np.sum(diag_z), "expected", self.magic_sum)
+                return False
+
+        # If all checks pass, it's a perfect magic cube
+        return True
+
+    # -- INTERNAL FUNCTIONS --
+
+    def __get_index(self, x: int, y: int, z: int) -> int:
+        """
+        Returns the 1D index for the given x, y, z coordinates in the Magic Cube.
+
+        :return: The corresponding index in the 1D array.
+        """
+        if x < 0 or x >= self.size:
+            raise IndexError("Row index out of bounds")
+        if y < 0 or y >= self.size:
+            raise IndexError("Column index out of bounds")
+        if z < 0 or z >= self.size:
+            raise IndexError("Layer index out of bounds")
+
+        return x + y * self.size + z * self.size**2
+
+    def __get_side_diag_x_left(self, x: int) -> np.ndarray:
+        return np.array([self.data[self.__get_index(x, i, i)] for i in range(self.size)])
+
+    def __get_side_diag_x_right(self, x: int) -> np.ndarray:
+        return np.array([self.data[self.__get_index(x, i, self.size - 1 - i)] for i in range(self.size)])
+
+    def __get_side_diag_y_left(self, y: int) -> np.ndarray:
+        return np.array([self.data[self.__get_index(i, y, i)] for i in range(self.size)])
+
+    def __get_side_diag_y_right(self, y: int) -> np.ndarray:
+        return np.array([self.data[self.__get_index(self.size - 1 - i, y, i)] for i in range(self.size)])
+
+    def __get_side_diag_z_left(self, z) -> np.ndarray:
+        return np.array([self.data[self.__get_index(i, i, z)] for i in range(self.size)])
+
+    def __get_side_diag_z_right(self, z) -> np.ndarray:
+        return np.array([self.data[self.__get_index(i, self.size - 1 - i, z)] for i in range(self.size)])
